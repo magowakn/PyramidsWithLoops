943fc502d150b03ed95d8a57997a5118deed69fb
magowakn <magowakn@LCBLAB2230-17.uccob.uc.edu>
2019-09-25 18:09:48 -0400

    This assignment is about altering the code to fit 5 lines of a pyramid

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,25 +1,32 @@
-/* 
- * Bill Nicholson
- * nicholdw@ucmail.uc.edu
- * Printing pyramids using loops. It kind-of works. 
- * What I really want is
-        1
-       222
-      33333
-     4444444
-    555555555 
+/*Assignment 04
+ *Kirran Magowan
+ *magowakn@mail.uc.edu
+ *App Dev w/ JAVA
+ *IS4010
+ *Description: This assignment is about altering the code to fit 5 lines of a pyramid from 1 to 5.
+ *Citation: https://stackoverflow.com/questions/1427272/for-loop-construction-and-code-complexity
+ *Citation: https://stackoverflow.com/questions/6685665/system-out-println-vs-n-in-java
 */
 package main;
 
 public class Main {
 
-	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
-			}
-			System.out.println("");
-		}
-	}
+ public static void main(String[] args) {
+  printDesign(5); // this tells Eclipse the number of rows that it will output
+ }
+  public static void printDesign(int height){ // this code evaluates the printDesign text and looks at the integer for height in the code below for it to scan and produce an output for the variable int.
+
+  for (int i = 1, x = 1; i < height * 2; i += 2, x++) { // declares the integer i as 1, then states that x will be added to if it doesnt fit the conditions
+   int n = x % 10; // this tell the program to divide x by 10
+   for (int j = 0; j < height* 2 - 1 - i / 2; j++) // this adjusts the height of the pyramid if j is less than the height multiplied by 2 - 1 - i, then 1 is added to x each time after that
+     System.out.print(" ");
+   
+   
+     for (int j = 0; j < i; j++) // this states that the integer j will get one added to it every time if  the conditions fit
+      System.out.print(n); // this out puts the integer n for the system
+      System.out.print("\n"); // this out prints lines separately instead of doing this 5 times over
+
+   }
+  }
 }
+
